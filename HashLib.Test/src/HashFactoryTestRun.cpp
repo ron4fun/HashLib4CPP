ffe006e085f604e29a9053b934f77274c7548acd
Ronald <ron2tele@gmail.com>
2017-08-11 14:25:34 +0100

    Restructured

---
+++ HashLib.Test/src/HashFactoryTestRun.cpp
@@ -0,0 +1,8 @@
+#include "CryptoTests.h"
+#include "ChecksumTests.h"
+#include "Hash32Tests.h"
+#include "Hash64Tests.h"
+#include "Hash128Tests.h"
+#include "PBKDF2_HMACTests.h"
+
+
--- src/HashFactoryApp/HashFactoryApp.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-#include "Tests\CryptoTests.h"
-#include "Tests\ChecksumTests.h"
-#include "Tests\Hash32Tests.h"
-#include "Tests\Hash64Tests.h"
-#include "Tests\Hash128Tests.h"
-#include "Tests\PBKDF2_HMACTests.h"
-
-
