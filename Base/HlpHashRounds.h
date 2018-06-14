5cc7d47be0fab36be722d1147cbd080d7917cb38
Mbadiwe Nnaemeka Ronald <ron2tele@gmail.com>
2018-06-14 23:17:31 +0100

    Added licence note

--- Base/HlpHashRounds.h
+++ Base/HlpHashRounds.h
@@ -1,3 +1,18 @@
+// ///////////////////////////////////////////////////////////////// //
+// *C++ 11 HashFactory Library                                 
+// *Copyright(c) 2018  Mbadiwe Nnaemeka Ronald                 
+// *Github Repository <https://github.com/ron4fun>             
+
+// *Distributed under the MIT software license, see the accompanying file LICENSE 
+// *or visit http ://www.opensource.org/licenses/mit-license.php.           
+
+// *Acknowledgements:                                  
+// ** //
+// *Thanks to Ugochukwu Mmaduekwe (https://github.com/Xor-el) for his creative        
+// *development of this library in Pascal/Delphi                         
+
+// ////////////////////////////////////////////////////// ///////////////
+
 #ifndef HLPHASHROUNDS_H
 #define HLPHASHROUNDS_H
 
